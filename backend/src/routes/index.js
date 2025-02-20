ea16a2efc130e4123969f599798b6c6441baadbf
ronanmacedo <ronan.macedo.edu@gmail.com>
2025-02-20 00:00:06 +0000

    refactor: remove unused client routes, validators, and components; update application structure

--- backend/src/routes/index.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-
-router.use('/health', require('./healthcheck.routes'));
-
-router.use('/accounts', require('./accounts.routes'));
-
-module.exports = router;
